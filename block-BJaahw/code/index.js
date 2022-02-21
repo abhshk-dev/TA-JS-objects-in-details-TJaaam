6e1781853b8527b142c3fca879de52f30ea45820
Abhishek Agarwal <abhishekagarwal151@gmail.com>
2022-02-21 12:56:01 +0530

    Block-4 final push class and objects

--- block-BJaahw/code/index.js
+++ block-BJaahw/code/index.js
@@ -1,14 +1,46 @@
 // 1. Create a function `createUser` that accepts `name` and `age` and returns a new object with those properties.
-function createUser(name,age){
-    return {}
+/*function createUser(name,age){
+    let user={};
+    user.name=name;
+    user.age=age;
+
+    return user;
 }
 // 2. Add a method named `sayHello` inside the object created above. When called it should alert `Welcome {user}`. Replace `{user}` with the name of the user.
-
+function createUser(name,age){
+    let user={};
+    user.name=name;
+    user.age=age;
+    user.sayHello=function(){
+        alert(`Hello ${user.name}`);
+    };
+    return user;
+}*/
 // 3. Use the data (name, age) of two different person to create the object using `createUser`. Store the returned value in `personOne` and `personTwo`.
+
+//let personOne=createUser('Abhishek',24);
+//let personTwo=createUser('john',21);
 
 // 4. Change the code inside `createUser` in such a way that the methods `sayHello` doesn't have to be in all object. Use the prototypal nature. (Hint Object.create())
 
+//let userMethods ={
+  //  sayHello:function(){    alert(`Hello ${user.name}`);
+
+
 // 5. Convert the `createUser` function into Pseudoclassical pattern of object creation. Use `F.prototype` to store the methods. `F.prototype` means storing the methods in prototype property of the function.
+
+function CreateUser(name,age){
+    let user=Object.create(userMethods);
+    user.name=name;
+    user.age=age;
+
+    return user;
+}
+CreateUser.prototype={
+    sayHello:function(){
+        alert(`Hello ${this.name}`)
+    },
+};
 
 // 6. Use `new` to create two new objects with the data of two different person and re-assign the value of `personOne` and `personTwo`.
 
