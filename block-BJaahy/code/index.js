d4c46c785ccde7000052605fe6a6fce253e6ccd3
Abhishek Agarwal <abhishekagarwal151@gmail.com>
2022-02-21 16:05:53 +0530

    Block-2 initial push Classes

---
+++ block-BJaahy/code/index.js
@@ -0,0 +1,16 @@
+class Square{
+    constructor(side){
+        this.width=side;
+        this.height=side;
+    }
+     description(){
+        return alert`The square is ${width} x ${height}`;
+    }
+    calcArea(){
+        let area=this.width*this.height;
+        
+    }
+    area(){
+        return 
+    }
+}
