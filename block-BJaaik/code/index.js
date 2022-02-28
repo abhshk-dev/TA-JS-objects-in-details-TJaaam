d3591e318e9d50d9045fd9a1f9da6d4765317326
Abhishek Agarwal <abhishekagarwal151@gmail.com>
2022-02-28 17:00:20 +0530

    Inheritance Asiignment l-3 final push

--- block-BJaaik/code/index.js
+++ block-BJaaik/code/index.js
@@ -6,12 +6,59 @@ class Person{
     }
     eat(food){
         this.food=food;
-        return console.log(`I like to Eat ${this.food}`);
+        return console.log(`${this.name} likes to Eat ${this.food}`);
     }
     sleep(){
-        return console.log(`I sleep 8 hours a Day`);
+        return console.log(`${this.name} sleeps 8 hours a Day`);
     }
     walk(){
-        return console.log(`I can walk 3 kms at a time`);
+        return console.log(`${this.name} can walk 3 kms at a time`);
     }
 }
+
+
+class Player extends Person{
+    constructor(name,age,gender,sportsName){
+        super(name,age,gender);
+        this.sportsName=sportsName;
+    }
+    
+    play(){
+        return console.log(`${this.name} can Play`);
+    }
+}
+
+class Teacher extends Person{
+    constructor(name,age,gender,instituteName){
+        super(name,age,gender);
+        this.instituteName=instituteName;
+    }
+    teach(subjects=[]){
+        this.subjects=subjects;
+        return console.log(`${this.name} teaches these subjects ${this.subjects}`);
+    }
+}
+
+class Artist extends Person{
+    constructor(name,age,gender,kind){
+        super(name,age,gender);
+        this.kind=kind;
+    }
+    createArt(){
+        return console.log(`${this.name} creates Amazing Rap Music`);
+    }
+}
+
+
+class Cricketer extends Player{
+    constructor(name,age,gender,sportsName,teamName){
+        super(name,age,gender,sportsName);
+        this.teamName=teamName;
+    }
+    playCricket(){
+        return console.log(`${this.name} play cricket `);
+    }
+}
+
+let cricketer1= new Cricketer('Rahul Dravid',45,'male','Cricket','India');
+let artist1= new Artist('Eminem',50,'male','Hip-Hop');
