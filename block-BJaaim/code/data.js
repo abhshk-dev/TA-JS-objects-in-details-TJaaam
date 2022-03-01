9c235a2f15fd6ac8643e3c772286f6f1d32364c0
Abhishek Agarwal <abhishekagarwal151@gmail.com>
2022-03-01 19:17:37 +0530

    final push Quiz app

---
+++ block-BJaaim/code/data.js
@@ -0,0 +1,60 @@
+let quizCollection = [
+    {
+        title: `What temperature does water boils at`,
+        options: [
+            "50 degree celcius",
+            "25 degree celcius",
+            "100 degree celcius",
+            "150 degree celcius",
+
+        ],
+        answerIndex: 2,
+    },
+    {
+        title: `The hottest planet in the solar system`,
+        options: [
+            "Mercury",
+            "Venus",
+            "Mars",
+            "Jupiter",
+
+        ],
+        answerIndex :1,
+    },
+    {
+        title: `Where was the electricity supply first introduced in India`,
+        options: [
+            "Dehradun",
+            "Mumbai",
+            "Darjeeling",
+            "Chennai",
+
+        ],
+        answerIndex : 2,
+    },
+    {
+        title: `Friction can be reduced by changing from`,
+        options: [
+            "rolling to sliding",
+            "sliding to rolling",
+            "dynamic to static",
+            "potential energy to kinetic energy",
+
+        ],
+        answerIndex :1,
+    },
+    {
+        title: `Fire temple is the place of worship of which of the following religion?`,
+        options: [
+            "Taoism",
+            "Shintoism",
+            "Judaism",
+            "Zoroastrianism (Parsi Religion)",
+
+        ],
+        answerIndex :3,
+    },
+
+]
+
+
